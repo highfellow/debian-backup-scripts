e5887156ff517f7cc5fa0dd4effc8699d08bb253
root <root@backup.veraz.co.uk>
2014-03-10 11:16:04 +0000

    first commit

---
+++ bin/udev-backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# this is invoked by a udev rule: /usr/local/etc/80-usb-backup.rules
+
+. /usr/local/etc/backup.conf
+
+echo "---------------------" >> /tmp/udev-backup
+#env >> /tmp/udev-backup
+echo $ACTION >> /tmp/udev-backup
+
+if [ "$ACTION" == "add" ] ; then
+  # kick off the disk-change script
+  echo "run" >> /tmp/udev-backup
+  /usr/local/bin/run-disk-change &
+  disown -ha # disown that job before exiting
+else
+  # unmount the device
+  echo "unmount" >> /tmp/udev-backup
+  umount $DEVNAME
+fi
